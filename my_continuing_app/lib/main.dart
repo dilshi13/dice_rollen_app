b7baef7722bdefb7857ba55f3a58e2890b7384fb
dilshi13 <dilshikasasitharan97@gmail.com>
2024-11-23 20:17:45 +0530

    Getting the app Screen as white using Scaffold

---
+++ my_continuing_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+home: HelloWorldApp(),
+  ));
+}
+
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
